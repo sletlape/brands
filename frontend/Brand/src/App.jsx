476bfcc01126d83f16648460af83a50310fcc23b
Khumo Letlape <31062011+sletlape@users.noreply.github.com>
2023-10-21 18:48:18 +0200

    Frontend: basic Vite setup with fonts and bulma

---
+++ frontend/Brand/src/App.jsx
@@ -0,0 +1,27 @@
+export function App() {
+
+    return (
+        <>
+            <div>
+                <div>
+                    <div className='logo'>logo</div>
+                    <div className='nav'>nav</div>
+                    <div className='btn-lets-talk'>btn</div>
+                </div>
+                <div className='hero-section'>
+                    hero
+                </div>
+                <div className='what-we-do-section'>
+                    - what we do
+                </div>
+                <div className='case-studies-sectinon'>
+                    - case studies
+                </div>
+                <div className='brands-section'>
+                    - You'll be in company
+                </div>
+                <div className='footer-section'></div>
+            </div>
+        </>
+    );
+}
