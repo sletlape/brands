b9e40340c2e53d14af93f13d35afd20a3b21216f
Khumo Letlape <31062011+sletlape@users.noreply.github.com>
2023-10-21 16:59:21 +0200

    Backend: Creating POST API test

--- backend/brands-backend/test/app.test.js
+++ backend/brands-backend/test/app.test.js
@@ -3,7 +3,6 @@ const app = require('../app/app');
 
 describe('Brands API', () => {
     describe('GET /brands', () => {
-
         it('GET /brands should return 200', async () => {
             const response = await request(app).get('/brands');
             expect(response.statusCode).toBe(200);
@@ -63,6 +62,51 @@ describe('Brands API', () => {
         });
     });
 
-    // describe('POST /brands', () => { });
+    describe('POST /brands', () => {
+        it('POST /brands should return 201 and a JSON object with the new brand', async () => {
+            const newBrand = {
+                brandName: 'Volvo',
+                logoURL: './images/volvo.png',
+            };
+            const response = await request(app).post('/brands').send(newBrand);
+            expect(response.statusCode).toBe(201);
+            console.log(response);
+            expect(response.body).toEqual(
+                expect.objectContaining({
+                    _id: expect.any(String),
+                    brandName: newBrand.brandName,
+                    logoURL: newBrand.logoURL,
+                    published: expect.any(String)
+                })
+            );
+        });
 
+        it('POST /brands should return 400 if the brandName is not provided', async () => {
+            const newBrand = {
+                logoURL: './images/nothing.png',
+            };
+            const response = await request(app).post('/brands').send(newBrand);
+            expect(response.statusCode).toBe(400);
+        });
+
+        // it post return error if wrong endpoint is used
+        it('POST /brands should return 404 if the wrong endpoint is used', async () => {
+            const newBrand = {
+                brandName: 'Wrong',
+                logoURL: './images/wrong.png',
+            };
+            const response = await request(app).post('/brand').send(newBrand);
+            expect(response.statusCode).toBe(404);
+        });
+
+        it('POST /brands should return error 400 if the brandName already exists', async () => {
+            const newBrand = {
+                brandName: 'BMW',
+                logoURL: './images/bmw.png',
+            };
+            const response = await request(app).post('/brands').send(newBrand);
+            expect(response.statusCode).toBe(400);
+        });
+        
+    })
 });
