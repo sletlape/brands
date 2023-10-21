6456fe94aef67b2dcde73484ee2bf271aaa5aa11
Khumo Letlape <31062011+sletlape@users.noreply.github.com>
2023-10-21 21:24:08 +0200

    Frontend: adding Hero component

---
+++ frontend/Brand/src/components/Hero.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import '../styles/Hero.css'; // Import your Hero component's CSS file
+import heroImage from '../assets/hero-bg.png';
+
+const Hero = () => {
+    return (
+        <div className="hero">
+            {/* Background image */}
+            <img className="hero-background" src={heroImage} alt="Hero background" style={{ width: '100%', height: '100%' }} />
+
+            {/* Content container */}
+            <div className="hero-content">
+                <h1 className="hero-title">Live with Confidence</h1>
+                <p className="hero-text">Jose Mourinho brings confidence to pan-African Sanlam campaign</p>
+                <button className="button is-primary">View Project</button>
+            </div>
+        </div>
+    );
+};
+
+export default Hero;
