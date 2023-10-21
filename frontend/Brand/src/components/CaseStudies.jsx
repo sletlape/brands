2af1f7dc550b7542fb34bb9689c6519ffc72d563
Khumo Letlape <31062011+sletlape@users.noreply.github.com>
2023-10-21 23:04:19 +0200

    Frontend: adding WhatWeDo and CaseStudies components wrong images

---
+++ frontend/Brand/src/components/CaseStudies.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import ScrollCarousel from 'scroll-carousel-react';
+
+import '../styles/CaseStudies.css';
+
+import caseStudy1 from '../assets/olimpian.png'
+import caseStudy2 from '../assets/savings.png';
+import caseStudy3 from '../assets/skhokho.png';
+
+const CaseStudies = () => {
+    const caseStudies = [
+        {
+            title: 'The Olympian',
+            description: 'The only athlete in the world to do ber Olympic routine in 2020.',
+            image: caseStudy1,
+        },
+        {
+            title: 'The Savings Jar',
+            description: 'Grow your savings treasuer and grow your dragon.',
+            image: caseStudy2,
+        },
+        {
+            title: 'Skhokho seMali',
+            description: 'Helping South Africans become #CashCleva with Skhokho seMali',
+            image: caseStudy3,
+        },
+    ];
+
+    return (
+        <div className="case-studies">
+            <p className="section-title">Case Studies</p>
+
+            <div className="case-study-list">
+                <ScrollCarousel
+                    autoplay
+                    autoplaySpeed={2}
+                    speed={2}
+                    onReady={() => console.log('I am ready')}
+                >
+                    {caseStudies.map((caseStudy, index) => (
+                        <div className="case-study" key={index}>
+                            <img src={caseStudy.image} alt={`Case Study ${index + 1}`} />
+
+                            <div className="case-study-info">
+                                <h3 className="case-study-title">{caseStudy.title}</h3>
+                                <p className="case-study-description">{caseStudy.description}</p>
+                            </div>
+                        </div>
+                    ))}
+                </ScrollCarousel>
+            </div>
+        </div>
+    );
+};
+
+export default CaseStudies;
---
+++ frontend/Brand/src/components/WhatWeDo.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import '../styles/WhatWeDo.css';
+import webDev from '../assets/webDevIcon.svg';
+import uxDesign from '../assets/webDevIcon.svg';
+import mobile from '../assets/webDevIcon.svg';
+import blockchain from '../assets/webDevIcon.svg';
+
+const WhatWeDo = () => {
+    const services = [
+        {
+            title: 'Web Development',
+            description: 'We use cutting-edge web development technologies to help our clients fulfill their business goals through functional, reliable solutions.',
+            icon: webDev,
+        },
+        {
+            title: 'User Experience & Design',
+            description: 'Our complete web design services will bring your ideas to life and provide you with a sleek, high-performing product that elevates your business.',
+            icon: uxDesign,
+        },
+        {
+            title: 'Mobile app development',
+            description: 'Our extensive mobile development experience allows us to create custom native and cross-platform iOS and Android mobile solutions for our clients.',
+            icon: mobile,
+        },
+        {
+            title: 'Blockchain solutions',
+            description: 'We conduct market research to determine the optimal blockchain-based solutions to help you grow your company and achieve your business goals.',
+            icon: blockchain,
+        },
+    ];
+
+    return (
+        <div className="what-we-do">
+            <p className="section-title">What We Do</p>
+            <h1>We offer a complete range of bespoke design and development services to help you turn your ideas into digital masterpieces</h1>
+            <div className="service-list">
+                {services.map((service, index) => (
+                    <div className="service" key={index}>
+                        <img src={service.icon} alt={`Service ${index + 1}`} />
+                        <h4 className="service-title">{service.title}</h4>
+                        <p className="service-description">{service.description}</p>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default WhatWeDo;
